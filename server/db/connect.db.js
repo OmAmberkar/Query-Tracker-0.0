87a252bf19aa4b88e2975a7560e953cf6767bce5
Disha Chaudhari <dishachaudhari801@gmail.com>
2025-05-27 15:13:50 +0530

    normal changes

--- server/db/connect.db.js
+++ server/db/connect.db.js
@@ -1,18 +1,20 @@
-import mongoose from 'mongoose';
-import dotevn from 'dotenv';
+import mongoose from "mongoose";
+import dotevn from "dotenv";
 
 dotevn.config();
 
-const connectDB = async() => {
-    try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
-        console.log(`MongoDB connected !! Db host: ${connectionInstance.connection.host}`);
-    }
-    catch(error)
-    {
-        console.log("MongoDB connection error ", error);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(
+      `MongoDB connected !! Db host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
